59db50b553b182273d73b63b2103faf5770e4c20
Matthew Robb <mr.obb@outlook.com>
2018-03-21 19:18:25 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,6 @@ module.exports = {
     },
     'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-netlify-cms'
+    'gatsby-plugin-sass'
   ]
 }
